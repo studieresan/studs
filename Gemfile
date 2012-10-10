67d7befbd7f4181e97d54ca670dd89634755a100
Victor Hallberg <vigge19@gmail.com>
2012-10-10 12:08:02 +0200

    Create rails project

---
+++ Gemfile
@@ -0,0 +1,29 @@
+source 'https://rubygems.org'
+
+gem 'rails', '3.2.3'
+
+gem 'mysql2'
+
+gem 'rails_config'
+
+gem 'bcrypt-ruby'
+gem 'sorcery'
+
+gem 'responders'
+
+gem 'haml-rails'
+gem 'redcarpet'
+
+gem 'mini_magick'
+gem 'carrierwave'
+
+group :development do
+  gem 'capistrano', :require => nil
+  gem 'capistrano-shared_file', :require => nil
+  gem 'capistrano_colors', :require => nil
+end
+
+group :production do
+  gem 'unicorn'
+  gem 'yui-compressor'
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Studs::Application.config.secret_token = '04cafdb756ce3760d12e4c8dc03ae22a8ddbeed9350abc00f344b9d67e34c412176ab21de9e24df93dd23c06a5362084ab420c664bcd5adcaddb8b2492445a7e'
