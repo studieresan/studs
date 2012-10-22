35f719764748c65049dc22df8da92a60d773b11e
Victor Hallberg <vigge19@gmail.com>
2012-10-22 15:04:16 +0200

    Add useful methods to app controller and include ExtFormHelper

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,37 @@
 class ApplicationController < ActionController::Base
   helper MenuHelper
 
+  after_filter :flash_to_xhr_headers
+
   protect_from_forgery
+
+  protected
+
+  def not_authenticated
+    redirect_to login_path
+  end
+
+  def save_return_url(url = nil)
+    url = request.fullpath if url.nil?
+    session[:return_to_url] = url
+  end
+
+  def flash_to_xhr_headers
+    return if !request.xhr? || flash.empty?
+    flash_json = Hash[flash.map{|k,v| [k,ERB::Util.h(v)] }].to_json
+    response.headers['X-Messages'] = flash_json
+    flash.discard
+  end
+
+  def not_authenticated
+    redirect_to login_path
+  end
+  
+  def custom_log_error(err)
+    if err.kind_of? Exception
+      Rails.logger.error "ERROR(#{err.class}): #{err.message}\nSOURCE: #{err.backtrace.first}"
+    else
+      Rails.logger.error("ERROR: #{err}")
+    end
+  end
 end
---
+++ app/helpers/ext_form_helper.rb
@@ -0,0 +1,111 @@
+module ExtFormHelper
+  # Add custom ext_form_for and ext_fields_for methods which uses this form builder.
+  %w(form_for fields_for).each do |meth|
+    src = <<-end_src
+      def ext_#{meth}(object_name, *args, &proc)
+        options = args.last.is_a?(Hash) ? args.pop : {}
+        options.update(builder: ExtFormBuilder)
+        #{meth}(object_name, *(args << options), &proc)
+      end
+    end_src
+    module_eval src, __FILE__, __LINE__
+  end
+
+  class ExtFormBuilder < ActionView::Helpers::FormBuilder
+    helpers = field_helpers +
+              %w(date_select datetime_select time_select) +
+              %w(collection_select select country_select time_zone_select) -
+              %w(hidden_field label fields_for) # Don't decorate these
+
+    helpers.each do |name|
+      define_method(name) do |field, *args|
+        opts = args.last.is_a?(Hash) ? args.pop : {}
+        classes = [] # css classes for field
+
+        # Include explicitly specified css classes
+        if opts.include?(:class)
+          classes += opts[:class].is_a?(Array) ? opts[:class] : opts[:class].to_s.split(' ')
+        end
+
+        # Add field name and type class
+        unless opts.delete(:limit_classes)
+          classes << css_class_for_name(name)
+          classes << 'disabled' if opts.include?(:disabled) && opts[:disabled]
+        end
+
+        # Check errors
+        object = @template.instance_variable_get("@#{@object_name.to_s.sub(/\[.+/, '')}")
+        classes << 'error' if !object.nil? && 
+            object.kind_of?(ActiveRecord::Base) && !object.errors[field.to_sym].empty?
+
+        # Call the parent field generator
+        opts[:class] = classes.uniq.join(' ')
+        args << opts
+        super(field, *args)
+      end
+
+      # Field row wrapper with container and error message included
+      define_method(name + '_row') do |field, *args|
+        opts = args.last.is_a?(Hash) ? args.last : {}
+        classes = %w(input) # css classes for container element
+        classes += opts.delete(:row_class) if opts.include?(:row_class)
+
+        # Include classes describing the actual field name and type
+        classes << css_class_for_name(name)
+        classes << field.to_s unless name == 'submit'
+        classes << 'disabled' if opts.include?(:disabled) && opts[:disabled]
+        # Do not include these in the field as well
+        opts[:limit_classes] = true
+
+        label_opts = {}
+
+        # Include error message
+        if label_opts[:title] = error_on(field)
+          label_opts[:class] = 'error'
+          classes << 'error'
+        end
+
+        field_html = send(name.to_sym, field, *args)
+
+        # Place inputs for radio buttons and check boxes inside label
+        if %w(check_box radio_button).include?(name)
+          output = label(field, opts.delete(:label), label_opts) do
+            "#{field_html} #{i18n_field_name(field)}".html_safe
+          end
+        else
+          output = label(field, opts.delete(:label), label_opts) + ' ' + field_html
+        end
+
+        @template.content_tag(:div, output, :class => classes.join(' '))
+      end
+    end
+
+    private
+
+    def i18n_field_name(field)
+      I18n.t("attributes.#{field}")
+    end
+
+    def object
+      # @template.instance_variable_get("@#{@object_name.to_s.sub(/\[.+/, '')}")
+      @object ||= @template.instance_variable_get("@#{@object_name}")
+    end
+
+    def error_on(field)
+      return nil if object.nil? || !object.respond_to?(:errors)
+      errors = object.errors[field.to_sym]
+      return nil if !errors || errors.empty?
+      errors.is_a?(Array) ? errors.first : errors
+    end
+
+    def css_class_for_name(name)
+      name = name.sub(/_(field|select)\Z/i, '')
+      return case name
+      when 'password' then 'text'
+      when 'check_box' then 'check'
+      when 'radio_button' then 'radio'
+      else name
+      end
+    end
+  end
+end
