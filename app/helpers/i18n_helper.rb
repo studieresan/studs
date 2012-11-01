d193cea1039533674d47dced9fb0c89ce9d267f3
Victor Hallberg <vigge19@gmail.com>
2012-11-02 00:26:21 +0100

    Add t_experience_kind i18n helper along with documentation of the whole module

--- app/helpers/i18n_helper.rb
+++ app/helpers/i18n_helper.rb
@@ -1,14 +1,26 @@
 module I18nHelper
+  # Returns a localized version of the provided attribute name.
+  # Lookups are done in attributes.{scope}.{attribute}, falling back to
+  # attributes.defaults.{attribute} if the former isn't defined.
   def t_attribute(*scope, attribute)
     default = scope.empty? ? nil : "attributes.defaults.#{attribute}"
     scope = scope.empty? ? 'defaults' : scope.join('.')
     I18n.t("attributes.#{scope}.#{attribute}", default: default)
   end
 
+  # Returns a localized version of the provided masters code.
   def t_masters(name)
     I18n.t("values.masters.#{name}", default: name.upcase)
   end
 
+  # Returns a localized version of the provided experience kind.
+  def t_experience_kind(experience)
+    I18n.t("values.experience_kind.#{experience}", default: experience.capitalize)
+  end
+
+  # Returns a localized action-model string.
+  # The model name and action is guessed from controller name and action if
+  # left unspecified.
   def t_model_action(model_name = nil, action = nil)
     model_name ||= controller_name.singularize
     action ||= action_name
