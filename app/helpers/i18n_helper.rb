df69d58421dc8ebde436874e001c0efabbc672b2
Victor Hallberg <vigge19@gmail.com>
2012-11-01 23:58:41 +0100

    Add t_model_action helper which returns a translated action-model text

--- app/helpers/i18n_helper.rb
+++ app/helpers/i18n_helper.rb
@@ -8,4 +8,10 @@ module I18nHelper
   def t_masters(name)
     I18n.t("values.masters.#{name}", default: name.upcase)
   end
+
+  def t_model_action(model_name = nil, action = nil)
+    model_name ||= controller_name.singularize
+    action ||= action_name
+    "#{I18n.t("actions.#{action}")} #{I18n.t("models.a.#{model_name}")}"
+  end
 end
