b90f65d7c8248434d30f750cecf02291b8ae18e4
Victor Hallberg <vigge19@gmail.com>
2012-11-01 17:06:48 +0100

    Make all tag links redirect to resume listing already filtered by tag

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -5,11 +5,18 @@ $(function() {
   // Resume form tag input
   $('#resume_skill_list').suggester().tagger();
 
-  // Resume search form
-  $('form.resume-search').each(function() {
-    var $form = $(this);
-    $form.find('input#s').suggester().tagger({
+  // Resume search form & listing
+  var $searchForm = $('form.resume-search');
+  if ($searchForm.length) {
+    var $tagger = $searchForm.find('input#s').suggester().tagger({
       allowDragging: false
     });
-  })
+
+    var $table = $('table.resumes');
+    $table.on('click', 'a.tag, a.itag', function() {
+      console.log("Add: " + $(this).text());
+      $tagger.tagger('addTag', $(this).text());
+      return false;
+    });
+  }
 });
