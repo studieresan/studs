028c4357a2ff1fe6dc5619bca2a8ebc6ab04b898
Victor Hallberg <vigge19@gmail.com>
2012-11-02 00:00:47 +0100

    Add boilerplate for experiences controller and views

---
+++ app/controllers/experiences_controller.rb
@@ -0,0 +1,15 @@
+class ExperiencesController < ApplicationController
+  respond_to :html, :xml
+  responders :flash, :collection
+
+  before_filter :require_login
+
+  load_and_authorize_resource :resume
+  load_and_authorize_resource :experience, through: :resume
+
+  def new
+  end
+
+  def edit
+  end
+end
