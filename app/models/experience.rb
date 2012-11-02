d8a9bb3dbe9d1fc42d3b38792abf81091eb18afd
Victor Hallberg <vigge19@gmail.com>
2012-11-02 01:44:51 +0100

    Add duration and duration_in_months to Experience

--- app/models/experience.rb
+++ app/models/experience.rb
@@ -8,7 +8,17 @@ class Experience < ActiveRecord::Base
   default_scope order('end_date DESC')
 
   def duration
-    [start_date, end_date || :present]
+    start = I18n.l(start_date, format: :duration)
+    if end_date
+      I18n.t('values.duration.ended_html', start: start,
+        :end => I18n.l(end_date, format: :duration), months: duration_in_months)
+    else
+      I18n.t('values.duration.ongoing_html', start: start)
+    end.html_safe
+  end
+
+  def duration_in_months
+    (end_date.year*12+end_date.month) - (start_date.year*12+start_date.month) + 1
   end
 
   def to_s
