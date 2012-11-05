177e0e94146849d61cd7d84781749a1f8f947d11
Victor Hallberg <vigge19@gmail.com>
2012-11-05 02:36:23 +0100

    Add ability determining if specific menu items should be shown and allow students to upload files

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -2,17 +2,22 @@ class Ability
   include CanCan::Ability
 
   def initialize(user)
+    # Static pages available to all visitors
+    can :view, [:index, :resumes, :contact]
+
     return unless user # anonymous
 
-    # all users can view resumes and update their credentials
+    # All users can view resumes and update their credentials
     can :read, Resume
     can :read, User, id: user.id
     can :update, User, id: user.id
 
     if user.student?
+      # Students can create and manage their own resumes
       can :create, Resume
       can :manage, Resume, user_id: user.id
-      can :create, :files
+
+      can [:view, :index, :create], :files
     end
 
     if user.admin?
