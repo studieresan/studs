511c25e34da66b0444bc07938534f4b05b2b8ee7
Victor Hallberg <vigge19@gmail.com>
2012-11-05 04:39:00 +0100

    Enable YUI compressor in production, raise email delivery errors

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -13,6 +13,8 @@ Studs::Application.configure do
 
   # Compress JavaScripts and CSS
   config.assets.compress = true
+  config.assets.css_compressor = :yui
+  config.assets.js_compressor = :yui
 
   # Don't fallback to assets pipeline if a precompiled asset is missed
   config.assets.compile = false
@@ -49,7 +51,7 @@ Studs::Application.configure do
   # config.assets.precompile += %w( search.js )
 
   # Disable delivery errors, bad email addresses will be ignored
-  # config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
 
   # Enable threaded mode
   # config.threadsafe!
