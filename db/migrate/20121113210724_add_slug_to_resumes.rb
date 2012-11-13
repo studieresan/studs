d4777fb3d247ab4c13d4af5740a47abb068a89b2
Victor Hallberg <vigge19@gmail.com>
2012-11-13 22:30:46 +0100

    Auto initialize resume slugs in migration

--- db/migrate/20121113210724_add_slug_to_resumes.rb
+++ db/migrate/20121113210724_add_slug_to_resumes.rb
@@ -1,6 +1,12 @@
 class AddSlugToResumes < ActiveRecord::Migration
-  def change
+  def up
     add_column :resumes, :slug, :string
     add_index :resumes, :slug, unique: true
+    Resume.initialize_urls
+  end
+
+  def down
+    remove_index :resumes, :slug
+    remove_column :resumes, :slug
   end
 end
