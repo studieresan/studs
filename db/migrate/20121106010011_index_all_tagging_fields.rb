8f734bd2405abe08238700474ad91a747566091b
Victor Hallberg <vigge19@gmail.com>
2012-11-13 22:24:21 +0100

    Fix migration rollback for IndexAllTaggingFields

--- db/migrate/20121106010011_index_all_tagging_fields.rb
+++ db/migrate/20121106010011_index_all_tagging_fields.rb
@@ -5,7 +5,7 @@ class IndexAllTaggingFields < ActiveRecord::Migration
   end
 
   def down
-    remove_index :taggings, [:taggable_id, :taggable_type, :tag_id, :context], name: "index_taggings_on_keys"
+    remove_index :taggings, name: "index_taggings_on_keys"
     add_index :taggings, [:taggable_id, :taggable_type, :context]
   end
 end
