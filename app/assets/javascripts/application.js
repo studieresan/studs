b1f91a34503c0a3bdb3d64a799933c6176375920
Victor Hallberg <vigge19@gmail.com>
2013-05-02 21:17:00 +0200

    Replace latest image by instagram feed

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,1 +1,10 @@
 //= require countdown
+//= require jquery.instagram.js
+
+$(function() {
+  $('#instagram-feed').instagram({
+    hash: 'studs2013',
+    show: 6,
+    clientId: 'afcc1e598062435789439adb642a9933'
+  })
+});
