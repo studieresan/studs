3f279d2f41c63df037d829b6e5f7b26e2c38b19f
Victor Hallberg <victor.hallberg@advisa.se>
2013-10-22 14:32:08 +0200

    Fix user creation task

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,5 +4,8 @@
 # Create admin user
 name = 'admin'
 password = '4v8dfk'
-User.create({ login: name, email: 'admin@root.com', password: password, role: 'admin'}, as: :admin)
-STDOUT.puts "Created admin user with login '#{name}' and password '#{password}'"
+if User.create!({ login: name, email: 'admin@root.com', password: password, password_confirmation: password, role: 'admin'}, as: :admin)
+  STDOUT.puts "Created admin user with login '#{name}' and password '#{password}'"
+else
+  STDOUT.puts "Failed to create admin user!"
+end
