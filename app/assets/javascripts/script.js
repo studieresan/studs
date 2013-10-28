d0f9b1b82b4f5f749e390053f3428e249b97371c
Jonas Sköld <jonas.skold.89@gmail.com>
2013-10-29 00:45:16 +0100

    Fixed nav-bar

--- app/assets/javascripts/script.js
+++ app/assets/javascripts/script.js
@@ -5,9 +5,13 @@ $(function() {
       var target = $(this.hash);
       target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
       if (target.length) {
+        if ($('#navigation-bar .navbar-collapse.in').length == 1) { 
+          //If responsive menu is not collapsed, collapse it.
+          $('#navigation-bar .navbar-toggle').click();
+        }
         $('html,body').animate({
           scrollTop: target.offset().top - 70
-        }, 1000);
+        }, 500);
         return false;
       }
     }
