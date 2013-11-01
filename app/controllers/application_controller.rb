e2244f0ea341fae41ef2ea6ff192a8a53458f9ea
Per Classon <perwclasson@gmail.com>
2013-11-01 13:45:37 +0100

    Locale links fix

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -69,8 +69,8 @@ class ApplicationController < ActionController::Base
   # URL options
   def default_url_options(options={})
     {
-      locale: I18n.locale,
-      trailing_slash: true,
+      locale: ((I18n.locale == I18n.default_locale) ? nil : I18n.locale),
+      trailing_slash: false,
     }.merge(options)
   end
 end
