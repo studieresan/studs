25809e12ff28d5dba6992360b942ef6ba1e42e05
mbark <barksten@kth.se>
2013-12-28 14:44:35 +0100

    Download only resumes shown by current filter

--- app/controllers/resumes_controller.rb
+++ app/controllers/resumes_controller.rb
@@ -6,10 +6,35 @@ class ResumesController < ApplicationController
   before_filter :require_login, only: :mine
   load_and_authorize_resource except: [:index, :mine, :create], find_by: :slug
 
+  OUTPUT_DIR = File.join(Rails.root, 'tmp', 'resumes')
+
   def index
+    @filer_params = params.to_hash.slice(*%w(n name s skill_list))
     @filter = ResumeFilter.new(params.to_hash.slice(*%w(n name s skill_list)))
     @resumes = @filter.resumes
     authorize! :read, Resume
+
+    respond_to do |format|
+      format.html
+      format.zip {
+        zipfile_name = File.join(OUTPUT_DIR, "all_resumes.zip")
+
+        File.delete(zipfile_name) if File.exists?(zipfile_name)
+        Dir.mkdir(OUTPUT_DIR) unless File.directory?(OUTPUT_DIR)
+
+        Dir.chdir OUTPUT_DIR do
+          Zip::File.open(zipfile_name, Zip::File::CREATE) do |zipfile|
+            @resumes.each do |resume|
+              tex = TexResume.new(resume, I18n.locale)
+              path = tex.save
+
+              zipfile.add("#{tex.base_name}.pdf", path[0])
+            end
+          end
+        end
+        send_file zipfile_name, filename: "studs.zip", type: 'application/zip'
+      }
+    end
   end
 
   def mine
