0928676383d7d751d8145cbed9825b848c0f5466
Jonas Sköld <jonas.skold.89@gmail.com>
2014-02-14 13:21:19 +0100

    Revert deploy file

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,39 +1,113 @@
+require 'capistrano_colors'
+
 set :application, 'studs'
-set :repository, 'https://mbark@github.com/mogelbrod/studs'
+
+# Source repository
+set :repository, 'git@github.com:mogelbrod/studs.git'
 set :scm, :git
 
+# Specify branch to deploy with:
+# cap deploy -s branch={name}
 set :branch, fetch(:branch, 'master')
 
-webfactional_domain = 'studs.webfactional.com'
-role :app, webfactional_domain, :alias => 'webfactional'
-role :web, webfactional_domain, :alias => 'webfactional'
-role :db,  webfactional_domain, :alias => 'webfactional', :primary => true
+# Stages
+set :stages, %w(production)
+set :default_stage, 'production'
+require 'capistrano/ext/multistage'
 
-set :user, "studs"
-set :scm_username, "mogelbrod"
+# Deploy target
+set :deploy_to, "/var/rails/#{application}"
+set :tmp_path, '/var/tmp/rails'
+
+# System
+set :user, 'rails'
 set :use_sudo, false
-default_run_options[:pty] = true
+set :ssh_options, { :forward_agent => true }
 
-set :deploy_to, "/home/studs/webapps/studs_rails"
+# Number of versions to keep
+set :keep_releases, 10
+after 'deploy:update_code', 'deploy:cleanup'
 
-# if you want to clean up old releases on each deploy uncomment this:
-# after "deploy:restart", "deploy:cleanup"
+# Roles
+clusterfluff_domain = 'clusterfluff.ben-and-jerrys.stacken.kth.se'
+role :app, clusterfluff_domain, :alias => 'clusterfluff'
+role :web, clusterfluff_domain, :alias => 'clusterfluff'
+role :db,  clusterfluff_domain, :alias => 'clusterfluff', :primary => true
 
-# if you're still using the script/reaper helper you will need
-# these http://github.com/rails/irs_process_scripts
+set :rails_env, 'production'
+default_environment['RAILS_ENV'] = 'production'
+#default_run_options[:shell] = 'bash'
 
+# Bundler
+set :bundle_cmd, '/usr/local/bin/1.9.2_bundle'
+require 'bundler/capistrano'
+
+# Whenever gem CRON jobs
+set :whenever_command, "#{bundle_cmd} exec whenever"
+set :whenever_environment, defer { stage }
+set :whenever_identifier, defer { "#{application}_#{stage}" }
+require 'whenever/capistrano'
+
+# Additional shared paths
+set :shared_children, shared_children + %w(public/uploads)
+set :shared_files, %w(config/database.yml config/email.yml config/unicorn.rb)
+set(:shared_file_path) { shared_path }
+require 'capistrano/shared_file'
+  
 namespace :deploy do
-  desc "Restart nginx"
-  task :restart do
-    run "#{deploy_to}/bin/restart"
+  desc "Deploy"
+  task :default do
+    update
+    restart
+    clear_cache
+  end
+
+  desc "Restart server (kill the unicorns)"
+  task :restart, :except => { :no_release => true } do
+    pid = "#{shared_path}/pids/unicorn.pid"
+    run "test -e #{pid} && kill -HUP `cat #{pid}` || /bin/true"
+    pid = "#{shared_path}/pids/unicorn.6050.pid"
+    run "test -e #{pid} && kill -HUP `cat #{pid}` || /bin/true"
+  end
+
+  desc "Reset database"
+  task :reset do
+    run "cd #{current_path} && RAILS_ENV=#{rails_env} #{bundle_cmd} exec rake db:reset"
+  end
+
+  desc "Run database migrations"
+  task :migrate, :except => {:no_release => true} do
+    run "cd #{current_path} && RAILS_ENV=#{rails_env} #{bundle_cmd} exec rake db:migrate"
+  end
+
+  desc "Update code and deploy with migrations applied"
+  task :migrations do
+    transaction do
+      update_code
+    end
+    migrate
+    restart
+  end
+
+  desc "Clear cached objects"
+  task :clear_cache do
+    run "cd #{current_path} && RAILS_ENV=#{rails_env} #{bundle_cmd} exec rake resumes:clean"
+  end
+
+  desc "Seed the database"
+  task :seed do
+    run "cd #{current_path} && RAILS_ENV=#{rails_env} #{bundle_cmd} exec rake db:seed"
   end
 end
 
-# If you are using Passenger mod_rails uncomment this:
-# namespace :deploy do
-#   task :start do ; end
-#   task :stop do ; end
-#   task :restart, :roles => :app, :except => { :no_release => true } do
-#     run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
-#   end
-# end
+# Shorten remote output prefixes
+class Capistrano::ServerDefinition
+  def to_s
+    @to_s ||= begin
+      s = @options[:alias] || host
+      s = "#{user}@#{s}" if user
+      s = "#{s}:#{port}" if port && port != 22
+      s
+    end
+  end
+end
