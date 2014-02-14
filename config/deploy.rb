77911a1cbeb1710ae613c4d340e2875ea80a4d34
mbark <barksten@kth.se>
2014-02-14 12:36:26 +0100

    removed strange stuff

--- config/deploy.rb
+++ config/deploy.rb
@@ -27,19 +27,10 @@ namespace :deploy do
 	task :restart do
 		run "#{deploy_to}/bin/restart"
 	end
-
-	namespace :assets do
-		task :precompile, :roles => :web, :except => { :no_release => true } do
-			from = source.next_revision(current_revision)
-			if capture("cd #{latest_release} && #{source.local.log(from)} vendor/assets/ app/assets/ | wc -l").to_i > 0
-				run %Q{cd #{latest_release} && #{rake} RAILS_ENV=#{rails_env} #{asset_env} assets:precompile}
-			else
-				logger.info "Skipping asset pre-compilation because there were no asset changes"
-			end
-		end
-	end
 end
 
+# vi borde kanske köra något med rake db:migrate
+# någonting med att byta namn på database.yml.dist -> database.yml
 
 # If you are using Passenger mod_rails uncomment this:
 # namespace :deploy do
