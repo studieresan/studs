dc811f514e09655153150ba7f442386855125528
mbark <barksten@kth.se>
2014-02-15 14:08:42 +0100

    Now it's possible to deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -4,6 +4,11 @@ set :scm, :git
 
 set :branch, fetch(:branch, 'webfaction-deploy')
 
+set :stages, %w(production)
+set :default_stage, 'production'
+require 'capistrano/ext/multistage'
+
+
 webfactional_domain = 'studs.webfactional.com'
 role :app, webfactional_domain, :alias => 'webfactional'
 role :web, webfactional_domain, :alias => 'webfactional'
@@ -14,7 +19,13 @@ set :scm_username, "mbark"
 set :use_sudo, false
 default_run_options[:pty] = true
 
+set :rails_env, 'production'
+default_environment['RAILS_ENV'] = 'production'
+
 set :deploy_to, "/home/studs/webapps/rails"
+
+set :bundle_cmd, "/home/studs/webapps/rails/bin/bundle"
+require 'bundler/capistrano'
 
 # if you want to clean up old releases on each deploy uncomment this:
 # after "deploy:restart", "deploy:cleanup"
@@ -22,10 +33,15 @@ set :deploy_to, "/home/studs/webapps/rails"
 # if you're still using the script/reaper helper you will need
 # these http://github.com/rails/irs_process_scripts
 
+set :default_environment, {
+  'PATH' => "#{deploy_to}/bin:$PATH",
+  'GEM_HOME' => "#{deploy_to}/gems" 
+}
+
 namespace :deploy do
 	desc "Restart nginx"
 	task :restart do
-		run "#{deploy_to}/bin/restart"
+		run "/home/studs/webapps/rails/bin/restart"
 	end
 end
 
