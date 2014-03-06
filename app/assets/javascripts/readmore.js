05da481e0eece3f7f4e61f8b611826687c772254
Per Classon <perwclasson@gmail.com>
2014-03-06 11:23:34 +0100

    Read more close on blog

--- app/assets/javascripts/readmore.js
+++ app/assets/javascripts/readmore.js
@@ -1,8 +1,17 @@
 $(function() {
-    $('.post .read-more').each(function(i, e) {
-        $(e).on('click', function() {
-            $(e).parent().find('.post-content').css({maxHeight: '100%'});
-            $(e).hide();
+    $('.post').each(function(i, e) {
+        var maxHeight = $(e).find('.post-content').css('maxHeight');
+        var readMoreOpen = $(e).find('.read-more-open');
+        var readMoreClose= $(e).find('.read-more-close');
+        readMoreOpen.on('click', function() {
+            $(e).find('.post-content').css({maxHeight: '100%'});
+            readMoreOpen.addClass("hide");
+            readMoreClose.removeClass("hide");
         });
+        readMoreClose.on('click', function() {
+            $(e).find('.post-content').css({maxHeight: maxHeight});
+            readMoreOpen.removeClass("hide");
+            readMoreClose.addClass("hide");
+        })
     });
 });
