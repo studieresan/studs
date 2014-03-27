a5038fc456a70a6039a37060d463ab88b40580d4
Jonas Sköld <jonas.skold.89@gmail.com>
2014-03-27 15:09:44 +0100

    hotfix for charts

--- app/assets/javascripts/charts.js
+++ app/assets/javascripts/charts.js
@@ -57,7 +57,7 @@ function handleQueryResponse(response, id, options) {
 	var data = response.getDataTable();
 	var opts = $.extend(true, //deep extension
 	{
-		title: data.yf[0].label,
+		title: data.Af[0].label,
 		titleTextStyle: {fontSize: 15, fontName: 'Avenir', color: '#333333'},
         colors: ['#EC1976', '#B03AF6', '#4C40DF', '#3A91F6', '#3EBFBF'],
         pieHole: 0.6,
