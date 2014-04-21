046c2895ef0b093c49d0687ed702f66f121610d9
Jonas Sköld <jonas.skold.89@gmail.com>
2014-04-21 15:25:18 +0200

    Blog posts ordered by created_at

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,7 +3,7 @@ class Post < ActiveRecord::Base
   accepts_nested_attributes_for :post_image,
     :allow_destroy => true,
     :reject_if     => :all_blank
-  default_scope lambda { order("updated_at DESC")}
+  default_scope lambda { order("created_at DESC")}
   attr_accessible :content, :published, :title, :post_image_attributes
 
   def to_s
