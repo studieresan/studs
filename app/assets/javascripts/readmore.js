c1a2acf40a9698e0bd5e8b6c9aa2cdd9219b659d
Dexter Gramfors <dexter.gramfors@gmail.com>
2014-08-17 10:25:42 +0200

    Changed behaviour in the newsfeed so that clicking read more triggers a nice animation.

--- app/assets/javascripts/readmore.js
+++ app/assets/javascripts/readmore.js
@@ -1,17 +1,32 @@
 $(function() {
     $('.post').each(function(i, e) {
-        var maxHeight = $(e).find('.post-content').css('maxHeight');
+        var postContent = $(e).find('.post-content');
+        var collapsedHeight = postContent.height();
+        var maxHeight = postContent.css('maxHeight');
+        postContent.css({maxHeight: '100%'}); // Set max-height to 100% in order to be able to get expanded height
+        var expandedHeight = postContent.css('height', '100%').height();
+        postContent.css('height', collapsedHeight); // Reset to correct (collapsed) height
         var readMoreOpen = $(e).find('.read-more-open');
-        var readMoreClose= $(e).find('.read-more-close');
+        var readMoreClose = $(e).find('.read-more-close');
         readMoreOpen.on('click', function() {
-            $(e).find('.post-content').css({maxHeight: '100%'});
             readMoreOpen.addClass("hide");
             readMoreClose.removeClass("hide");
+            postContent.animate({
+                height: expandedHeight + "px"
+            }, 1000);
         });
         readMoreClose.on('click', function() {
-            $(e).find('.post-content').css({maxHeight: maxHeight});
             readMoreOpen.removeClass("hide");
             readMoreClose.addClass("hide");
-        })
-    });
+            // Only scroll if the expanded height is bigger than collapsed height
+            if (parseInt(expandedHeight) > parseInt(collapsedHeight)) {
+                $('html, body').animate({
+                    scrollTop: postContent.offset().top
+                }, 1000);
+            }
+            postContent.animate({
+                height: collapsedHeight + "px"
+            }, 1000);
+        });
+    })
 });
