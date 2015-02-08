a514bd8d43835b8b35926c5a687709d55a996510
Dexter Gramfors <dexter.gramfors@gmail.com>
2015-02-08 14:55:36 +0100

    Added an OK-looking bubble chart for better visualisation of the 'describe your interpretation of the company' question. Responsiveness and general looks still not ideal.

--- app/assets/javascripts/charts.js
+++ app/assets/javascripts/charts.js
@@ -18,15 +18,21 @@ function drawCharts() {
 		$('#stats').html('<div class="col-xs-12">Efter eventet kommer statistik från medlemsenkäten att presenteras här.</div>');
 		return;
 	}
+	var sentenceChart;
 	for (var i = 0; i < charts.length; i++) {
 		var chart = charts[i];
-		var chartDiv = $('<div></div>').prop('id','chart-'+i).addClass('chart col-md-6');
-		$('#charts').append(chartDiv);
+		if (i === 3) {
+			sentenceChart = $('<div></div>').prop('id','chart-'+i).addClass('chart col-md-12');
+		} else {
+			var chartDiv = $('<div></div>').prop('id','chart-'+i).addClass('chart col-md-6');
+			$('#charts').append(chartDiv);
+		}
 		var queryString = null;
 		if (chart.column != undefined)
 			queryString = "SELECT " + chart.column + ", COUNT(A) GROUP BY " + chart.column;
-		drawPieChart(chart.url, queryString, "chart-" + i, chart.options);
+		drawChart(chart.url, queryString, "chart-" + i, chart.options);
 	}
+	$('#sentence-chart').append(sentenceChart);
 }
 
 /**
@@ -35,7 +41,7 @@ The queryString is used to filter the data of the spreadsheet, see
 https://developers.google.com/chart/interactive/docs/querylanguage
 The id is the id of the div that will contain the chart
 */
-function drawPieChart(url, queryString, id, options) {
+function drawChart(url, queryString, id, options) {
 	var query = new google.visualization.Query(url);
 	// Apply query language if specified
 	if (queryString != null)
@@ -59,22 +65,68 @@ function handleQueryResponse(response, id, options) {
 	var threeWordsQuestion = "Describe your interpretation";
 	if (startsWith(data.Pf[0].label, threeWordsQuestion)) {
 		data = buildCustomDatatable(data);
+		drawBubbleChart(data, id, options);
 	} else {
 		data = sortData(data);
+		drawPieChart(data, id, options);
 	}
+}
+
+function drawPieChart (data, id, options) {
 	var opts = $.extend(true, //deep extension
 	{
 		title: data.Pf[0].label,
 		titleTextStyle: {fontSize: 15, fontName: 'Avenir', color: '#333333'},
-        colors: ['#FF35BB', '#FFF43E', '#D8D8D8', '#3A91F6', '#3EBFBF'],
-        pieHole: 0.6,
+	      colors: ['#FF35BB', '#FFF43E', '#D8D8D8', '#3A91F6', '#3EBFBF'],
+	      pieHole: 0.6,
 		height: 300, 
 		pieSliceText: 'none', 
 		legend: {position: 'right', alignment: 'center', textStyle: 
 		{fontSize: 14, fontName: 'Avenir', color: '#333333'}},
 		chartArea: {width: '100%', height: '55%'}
 		}, options);//Add user supplied options
-	visualization = new google.visualization.PieChart(document.getElementById(id));
+	var visualization = new google.visualization.PieChart(document.getElementById(id));
+	visualization.draw(data, opts);
+}
+
+function drawBubbleChart (data, id, options) {
+	var opts = $.extend(true, //deep extension
+	{
+		title: data.Pf[0].label,
+		titleTextStyle: {fontSize: 15, fontName: 'Avenir', color: '#333333'},
+        colors: ['#FF35BB', '#FFF43E', '#D8D8D8', '#3A91F6', '#3EBFBF'],
+		height: 300,
+		vAxis: {
+			baselineColor: '#FFFFFF',
+		  gridlines: {
+		  	color: 'transparent'
+	    },
+	    viewWindow: {
+	    	min: 0,
+	    	max: 100
+	    }
+		},
+		hAxis: {
+			baselineColor: '#FFFFFF',
+			textPosition: 'none',
+	    gridlines: {
+	      color: 'transparent'
+	    },
+	    viewWindow: {
+	    	min: 0,
+	    	max: 100
+	    }
+		},
+		sizeAxis: {minValue: 0,  maxSize: 50},
+		legend: {position: 'right', alignment: 'center', textStyle: 
+		{fontSize: 14, fontName: 'Avenir', color: '#333333'}},
+		chartArea: {width: '100%', height: '55%'},
+		bubble: {textStyle: {auraColor: 'none', fontSize: 14, fontName: 'Avenir', color: '#333333'}},
+		}, options);//Add user supplied options
+  var number = data.getValue(1, 4);
+  var percentage = Math.round(number / 28 * 100);
+  data.setFormattedValue(1, 4, percentage + '% (' + number + '/' + 28 + ')');
+	var visualization = new google.visualization.BubbleChart(document.getElementById(id));
 	visualization.draw(data, opts);
 }
 
@@ -84,7 +136,8 @@ Insanely ugly custom hack to counter a specific question in the surveys of studs
 as a unique answser, instead of increasing the count for each sentence by one.
 */
 function buildCustomDatatable(originalData) {
-	var answers = {
+	var memberNo = 28;
+	var answerValues = {
 		'Interesting scope of practice' : 0,
 		'Interesting methodology' : 0,
 		'Optimal geographic location' : 0,
@@ -97,35 +150,60 @@ function buildCustomDatatable(originalData) {
 		'Flexibility' : 0,
 		'International possibilities' : 0
 	};
+
 	for (var i = 0; i < originalData.getNumberOfRows(); i++) {
 		var sentences = originalData.getValue(i, 0).split(', ');
 		var number = originalData.getValue(i, 1);
 		$.each(sentences, function (idx, sentence) {
-			answers[sentence] += number;
+			answerValues[sentence] += number;
 		})
 	};
+
 	var data = new google.visualization.DataTable(
     {
       cols: [{id: 'sentence', label: originalData.Pf[0].label, type: 'string'},
-             {id: 'number', label: 'Number', type: 'number'}]
+      			 {id: 'x', type: 'number'},
+      			 {id: 'y', type: 'number'},
+      			 {id: 'colorIndex', type: 'string'},
+      			 {id: 'number', label: 'Partition', type: 'number'}]
     });
-	$.each(answers, function (answer) {
-		data.addRow([answer, answers[answer]]);
-	})
+	var i = 1;
+	var mapKeys = Object.keys(answerValues);
+	mapKeys.sort(function(a,b){return answerValues[b] - answerValues[a]}); // Get the keys sorted by their values
+	// Extract the three most popular answers, set the formatted value for the tooltip
+	for (var i = 0; i < 3; i++) {
+		var key = mapKeys[i];
+		var value = answerValues[key];
+		var colorIndex = i == 0 ? '0' : '1';
+		var pos = getPosition(i);
+		data.addRow([key, pos.x, pos.y, colorIndex, value]);
+	  var percentage = Math.round(value / memberNo * 100);
+	  data.setFormattedValue(i, 4, percentage + '% (' + value + '/' + memberNo + ')');
+	};
+	data.sort({column: 4});
 	return data;
+}
+
+function getPosition (index) {
+	switch(index) {
+		case 0: return {x: 45, y: 69};
+		case 1: return {x: 34, y: 34};
+		case 2: return {x: 55, y: 23};
+	}
 }
 
 function sortData(data) {
 	var sortedData = data.clone();
 	var columnIndex = sortedData.addColumn('number', 'Priority');
 	for (var i = 0; i < sortedData.getNumberOfRows(); i++) {
-		if (isPositiveWord(sortedData.getValue(i,0))) {
+		var word = sortedData.getValue(i,0);
+		if (isPositiveWord(word)) {
 			sortedData.setValue(i, columnIndex, 1);
 		}
-		if (isNegativeWord(sortedData.getValue(i,0))) {
+		if (isNegativeWord(word)) {
 			sortedData.setValue(i, columnIndex, 2);
 		}
-		if (isNeutralWord(sortedData.getValue(i,0))) {
+		if (isNeutralWord(word)) {
 			sortedData.setValue(i, columnIndex, 3);
 		}
 	};
